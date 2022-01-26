64f9a54428c2dfda2a854f1a27caca08f7ba7e99
Wan-Min <uma123520@gmail.com>
2022-01-26 14:49:00 +0800

    add api route

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/clear/cache',function(){ 
+    Artisan::call('cache:clear');   
+    Artisan::call('config:clear');   
+    Artisan::call('config:cache');   
+    Artisan::call('route:clear');   
+    Artisan::call('view:clear');   
+    return response()->json([
+        'result' => true  
+    ]);
+});
