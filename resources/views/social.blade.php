b60e118088131a53801fc7435758f4f4377baf4d
Wan-Min <uma123520@gmail.com>
2022-02-15 13:19:31 +0800

    add todo

--- resources/views/social.blade.php
+++ resources/views/social.blade.php
@@ -2,6 +2,10 @@
 @section('page')
     <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+            TODO：
+            <ul>
+                <li>user login、register "Request" 未設定</li>
+            </ul>
             @if(Session::has('errors'))
                 {{Session::get('errors')->first()}}
             @endif
